7f526b7af80a9e0aa5a82221c72ed9af0feeff74
Christopher Henderson <chenderson5280@gmail.com>
2020-09-06 02:10:26 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,7 @@ end
 
 def monopoly_with_second_tier
 
-  hash = {:railroads => {}}
+  #hash = {:railroads => {}}
   hash[:railroads][:pieces] = 4
   hash [:railroads][:rent_in_dollars] = {}
   hash [:railroads][:names] = {}
